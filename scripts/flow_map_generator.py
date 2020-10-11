0afba916ddc5f884e6ea8e849cb6c893bd250572
venksrin09 <venksrin@pensando.io>
2020-10-11 19:39:18 +0000

    move to pkey option for ssh

--- scripts/flow_map_generator.py
+++ scripts/flow_map_generator.py
@@ -21,6 +21,7 @@ topology_file  = '../input/all_dscs_file.json'
 penctl_exe     = './penctl.linux'
 cert_file      = '/home/venksrin/.ssh/psm_token.pem'
 
+private_key_file         = '/home/venksrin/.ssh/id_rsa'
 
 # This script uses the 
 output_file    = '../DATA/flow_data.json'
@@ -39,7 +40,6 @@ with open( topology_file ) as fr:
 
 
 
-host_config = input_dict['host_dict']
 dsc_config = input_dict['dsc_dict']
 
 dsc_ip_list = []
@@ -47,7 +47,7 @@ for dsc_id in dsc_config.keys():
     dsc_ip_list.append( dsc_config[dsc_id]['mgmt_ip'] )
 
 # Create Parallel sessions ..
-phdl = ParallelSSHClient(dsc_ip_list, user='root', password=None )
+phdl = ParallelSSHClient(dsc_ip_list, user='root', pkey=private_key_file )
 
 # Generate flow_map_dict per DSC ..
 flow_dict = dsc_oper_lib.get_dscs_flow_dict(log, phdl )
